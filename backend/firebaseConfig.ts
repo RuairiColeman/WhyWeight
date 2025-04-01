dc6fc4006f51178a4c22b3bdc8d11a4e6c35578e
RuairiColeman <rcoleman1793@gmail.com>
2025-04-01 19:28:17 +0100

    added .env

--- backend/firebaseConfig.ts
+++ backend/firebaseConfig.ts
@@ -1,14 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDQbomBfzWhAWl0PfZeUGJj1ME_JmvmQyI",
-  authDomain: "whyweight-81b4a.firebaseapp.com",
-  projectId: "whyweight-81b4a",
-  storageBucket: "whyweight-81b4a.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef123456",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
